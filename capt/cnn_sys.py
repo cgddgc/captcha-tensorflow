ec3f564ced4b822a3c475903fc2a1ad1d4926a44
cgddgc <cgddgc@foxmail.com>
2017-12-12 17:31:17 +0800

    171212

--- capt/cnn_sys.py
+++ capt/cnn_sys.py
@@ -29,26 +29,38 @@ def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
     w_c1 = tf.Variable(w_alpha * tf.random_normal([3, 3, 1, 32]))
     b_c1 = tf.Variable(b_alpha * tf.random_normal([32]))
     conv11 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x, w_c1, strides=[1, 1, 1, 1], padding='SAME'), b_c1))
+    #用3x3x1卷积核对图像进行卷积
     conv12 = tf.nn.max_pool(conv11, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    #池化操作，图像宽高变为一半（向上取整）
     conv13 = tf.nn.dropout(conv12, keep_prob)
+    #防止过拟合
+    
+
 
     w_c2 = tf.Variable(w_alpha * tf.random_normal([3, 3, 32, 64]))
     b_c2 = tf.Variable(b_alpha * tf.random_normal([64]))
     conv21 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv13, w_c2, strides=[1, 1, 1, 1], padding='SAME'), b_c2))
+    #用3x3x1卷积核对图像进行卷积
     conv22 = tf.nn.max_pool(conv21, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    #池化操作，图像宽高变为一半（向上取整）
     conv23 = tf.nn.dropout(conv22, keep_prob)
+    #防止过拟合
+    
+
 
     w_c3 = tf.Variable(w_alpha * tf.random_normal([3, 3, 64, 64]))
     b_c3 = tf.Variable(b_alpha * tf.random_normal([64]))
     conv31 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv23, w_c3, strides=[1, 1, 1, 1], padding='SAME'), b_c3))
+    #用3x3x1卷积核对图像进行卷积
     conv32 = tf.nn.max_pool(conv31, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    #池化操作，图像宽高变为一半（向上取整）
     conv33 = tf.nn.dropout(conv32, keep_prob)
+    #防止过拟合
 
-    # Fully connected layer
+    # 最后一层全连接
     w_d = tf.Variable(w_alpha * tf.random_normal([3*6*64, 1024]))
     b_d = tf.Variable(b_alpha * tf.random_normal([1024]))
     dense = tf.reshape(conv33, [-1, w_d.get_shape().as_list()[0]])
-    #print(dense)
     dense = tf.nn.relu(tf.add(tf.matmul(dense, w_d), b_d))
     dense = tf.nn.dropout(dense, keep_prob)
 
